2166214e72bd89f127b5959a7347661117257108
Irbaz2004 <irbazahmeds20@gmail.com>
2026-03-20 17:47:41 +0530

    change

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:4000", {
+export const socket = io("https://server-tndf.onrender.com", {
   autoConnect: true,
   reconnection: true,
   reconnectionAttempts: 5,
